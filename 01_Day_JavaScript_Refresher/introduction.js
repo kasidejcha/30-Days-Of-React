5c393308c0756a59cfc29e0d98bd2ba2e9f00bfe
kasidejcha <kasidejcha.bas@gmail.com>
2023-02-11 11:38:08 +0700

    array

--- 01_Day_JavaScript_Refresher/introduction.js
+++ 01_Day_JavaScript_Refresher/introduction.js
@@ -1,10 +1,1 @@
 console.log('Introduction to Javascript');
-
-// 1. Create a variable
-var myName = 'John Doe';
-console.log(myName);
-
-const myAge = 25;
-console.log(myAge);
-const myAge = 26;
-console.log(myAge);
